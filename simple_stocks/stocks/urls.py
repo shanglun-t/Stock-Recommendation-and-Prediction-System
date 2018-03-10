067e2770844bd8a1ecdc08461e3d017c02fa179b
shanglun-t <33112379+shanglun-t@users.noreply.github.com>
2018-03-10 13:15:56 -0800

    Update urls.py

--- simple_stocks/stocks/urls.py
+++ simple_stocks/stocks/urls.py
@@ -1,7 +1,8 @@
 """Defines URL patterns for stocks"""
-
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #Home page
@@ -13,13 +14,6 @@ urlpatterns = [
     url(r'^q_start/$', views.q_start, name='q_start'),
     url(r'^q_formB/$', views.B_form, name='q_formB'),
     url(r'^q_formF/$', views.F_form, name='q_formF'),
-    
-    #url(r'^q_01', views.q_01, name='q_01'),
-    #url(r'^q_02', views.q_02, name='q_02'),
-    #url(r'^q_03', views.q_03, name='q_03'),
-    #url(r'^q_04', views.q_04, name='q_04'),
-    #url(r'^q_05A', views.q_05A, name='q_05A'),
-    #url(r'^q_05B', views.q_05A, name='q_05B'),
-    #url(r'^q_05C', views.q_05A, name='q_05C'),
-    #url(r'^q_05D', views.q_05A, name='q_05D'),
-]
+    url(r'^q_resultB/$',views.q_resultB, name='q_resultB'),
+    url(r'^q_resultF/$',views.q_resultF, name='q_resultF'),  
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
