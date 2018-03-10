1a474896887342d77bf2e445125d7f03158cb31f
shanglun-t <33112379+shanglun-t@users.noreply.github.com>
2018-03-10 13:14:16 -0800

    Update settings.py

--- simple_stocks/simple_stocks/settings.py
+++ simple_stocks/simple_stocks/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    ('/Users/shangluntsai/Capstone/simple_stocks/stocks/static/'),
+]
