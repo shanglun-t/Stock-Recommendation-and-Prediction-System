9ec9d505387a77372ad1fd53e298fd673f576bce
shanglun-t <33112379+shanglun-t@users.noreply.github.com>
2018-03-13 04:57:41 -0700

    Update settings.py

--- simple_stocks/simple_stocks/settings.py
+++ simple_stocks/simple_stocks/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     ('/Users/user/Capstone/simple_stocks/stocks/static/'),
