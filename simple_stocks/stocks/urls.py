f78632cf65b363311d8919fbc52c11d2eb8d6576
shanglun-t <33112379+shanglun-t@users.noreply.github.com>
2018-03-24 11:54:59 -0700

    Update urls.py

--- simple_stocks/stocks/urls.py
+++ simple_stocks/stocks/urls.py
@@ -1,19 +1,19 @@
 """Defines URL patterns for stocks"""
 from django.conf.urls import url
-from . import views
+from stocks.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     #Home page
-    url(r'^$', views.home, name='home'),
-    url(r'^home/$', views.home, name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^home/$', HomeView.as_view(), name='home'),
     #Disclaimer
-    url(r'^q_disclaimer/$', views.q_disclaimer, name='q_disclaimer'),
+    url(r'^q_disclaimer/$', q_disclaimer.as_view(), name='q_disclaimer'),
     #Questionnaire
-    url(r'^q_start/$', views.q_start, name='q_start'),
-    url(r'^q_formB/$', views.B_form, name='q_formB'),
-    url(r'^q_formF/$', views.F_form, name='q_formF'),
-    url(r'^q_resultB/$',views.q_resultB, name='q_resultB'),
-    url(r'^q_resultF/$',views.q_resultF, name='q_resultF'),  
+    url(r'^q_start/$', q_start.as_view(), name='q_start'),
+    url(r'^q_formB/$', B_form.as_view(), name='q_formB.'),
+    url(r'^q_formF/$', F_form.as_view(), name='q_formF.'),
+
+    #url(r'^q_resultF/$',views.q_resultF, name='q_resultF'),
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
