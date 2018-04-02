4f4651337004e2a01a2015346f25ad02b4d9a281
Shang Lun Tsai <shanglun.t@gmail.com>
2018-04-02 09:47:43 -0700

    Update DailyUpdate.py

--- simple_stocks/Back-End/DailyUpdate.py
+++ simple_stocks/Back-End/DailyUpdate.py
@@ -1,5 +1,4 @@
-import csv 
-import sys
+import csv
 from collections import OrderedDict
 import numpy as np
 import scipy as sp
@@ -8,7 +7,6 @@ from functools import reduce
 import string
 
 count = 0
-
 
 sumOfClose = np.array([])
 directory = os.path.join("Users/jayb/Desktop/srps/", "historical_data")
@@ -21,36 +19,40 @@ for root, dirs,files in os.walk("historical_data"):
                 adjReturn = np.array([])
                 for row in reader:
                     try:
-                         adj = (next(reader))
-                         adj = float(adj[6])
-                         adjReturn = np.append (adjReturn, (float(row[6])/adj -1 ))
+                        adj = (next(reader))
+                        adj = float(adj[5])
+                        adjReturn = np.append(adjReturn, (float(row[5]) / adj - 1))
                     except:
                         pass
-                #simpleReturn = (closePrice[-1] - closePrice[0])/closePrice[0]
-                #annualReturn = (simpleReturn + 1)**((1 / 10)-1)
+                # simpleReturn = (closePrice[-1] - closePrice[0])/closePrice[0]
+                # annualReturn = (simpleReturn + 1)**((1 / 10)-1)
 
                 sector = None
                 industry = None
-                
-                with open("workbook4.csv") as csvfile:
+
+                with open("workbook3.csv") as csvfile:
                     reader1 = csv.reader(csvfile, delimiter=',')
                     next(reader1)
-                    file = file [0:-4]
-                    
-                    for row in reader1:                                              
-                        if row[0]== file:
-                            sector = row [2]
+                    file = file[0:-4]
+
+                    for row in reader1:
+                        if row[0] == file:
+                            company = row[1]
+                            sector = row[2]
                             industry = row[3]
-                            
-                try:                       
-                    row = '\n' + str(file) + ', '+ str(sector) + ', '+ str(industry) + ', '+ str(np.var(adjReturn)) + ', '+ str(np.std(adjReturn)) +', '+ str(np.mean(adjReturn))  
+
+                try:
+                    row = '\n' + str(file) + ', ' + str(company) + ', ' + str(sector) + ', ' + str(
+                        industry) + ', ' + str(np.std(adjReturn)) + ', ' + str(np.var(adjReturn)) + ', ' + str(
+                        np.mean(adjReturn))
                 except:
                     pass
-                fd = open('lala.csv','a')
-                fd.write(row)                              
+                fd = open('industry_performance.csv', 'a')
+                fd.write(row)
                 count = count + 1
                 fd.close()
-                
-                #sumOfClose = np.append (sumOfClose, np.std(closePrice))
+
+                # sumOfClose = np.append (sumOfClose, np.std(closePrice))
                 closePrice = None
-print (count)
+print(count)
+                
