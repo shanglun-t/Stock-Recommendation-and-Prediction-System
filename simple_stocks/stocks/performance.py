2dff7e861c96ba9c757e9284cf2b1ac20a39b6a3
Shang Lun Tsai <shanglun.t@gmail.com>
2018-04-02 09:45:37 -0700

    Update performance.py

--- simple_stocks/stocks/performance.py
+++ simple_stocks/stocks/performance.py
@@ -14,16 +14,15 @@ class SelectionByPerformance:
         self.volatility = volatility
         self.risk = risk
         self.returns = returns
-        
 
     # returns list of Sector with value in range of self.high and self.low
     def reduceByValue(self, low, high, myDict):
         self.result = list()
         for k, v in myDict.items():
-                if  float(v) >= self.low and float(v) <= self.high :
-                    self.result.append (k)
+            if float(v) >= self.low and float(v) <= self.high:
+                self.result.append(k)
 
-        if  len(self.result) <= 2:
+        if len(self.result) <= 0:
             self.low = self.low - self.low * 0.3
             if len(self.result) <= 1:
                 self.low = self.low - self.low * 0.35
@@ -31,24 +30,21 @@ class SelectionByPerformance:
 
             self.high = self.high + self.high * 0.3
 
-
-
-
             for k, v in myDict.items():
                 if float(v) >= self.low and float(v) <= self.high:
                     self.result.append(k)
 
         return self.result
-    
-    # provides list of Sector based on volatility and risk chosen by the user 
+
+    # provides list of Sector based on volatility and risk chosen by the user
     def getPerformanceList(self):
 
         # create 5 groups of sector based on volatility
-        self.low = min (float(self.volatility[k]) for k in self.volatility)
-        self.high = max (float(self.volatility[k]) for k in self.volatility)
+        self.low = min(float(self.volatility[k]) for k in self.volatility)
+        self.high = max(float(self.volatility[k]) for k in self.volatility)
 
-        self.diff = (self.high - self.low)/5
-                      
+        self.diff = (self.high - self.low) / 5
+
         # select range of volatility based on user input
         if self.holdingPeriod == 'T1':
             self.low = self.low + self.diff * 4
@@ -60,59 +56,53 @@ class SelectionByPerformance:
             self.low = self.low + self.diff
         if self.holdingPeriod == 'T5':
             pass
-            
+
         self.high = self.low + self.diff
-        #print (' volatility high low',self.high, self.low)
+        # print (' volatility high low',self.high, self.low)
 
-        #reduce the list of sector based on the volatility range
-        self.result =self.reduceByValue(self.low, self.high ,self.volatility)
-        print ('Volatility', self.result)
+        # reduce the list of sector based on the volatility range
+        self.result = self.reduceByValue(self.low, self.high, self.volatility)
+        print('Volatility', self.result)
 
-        
         self.risk = {k: self.risk[k] for k in self.result if k in self.risk}
 
-         # create 2 groups of sector based on risk
-    
-        self.low = min (float(self.risk[k]) for k in self.risk)
-        self.high = max (float(self.risk[k]) for k in self.risk)
+        # create 2 groups of sector based on risk
 
-       
-        
-        self.diff = (self.high - self.low)/2
+        self.low = min(float(self.risk[k]) for k in self.risk)
+        self.high = max(float(self.risk[k]) for k in self.risk)
 
-        
+        self.diff = (self.high - self.low) / 2
+
         # select range of risk based on user input
         if self.riskLevel == 'R1' or self.riskLevel == 'R2':
             self.low = self.low + self.diff
         if self.riskLevel == 'R3' or self.riskLevel == 'R4':
             pass
 
-            
         self.high = self.low + self.diff
-        #print(' risk high low', self.high, self.low)
-        #reduce the list of sector based on the risk range
-        self.result =self.reduceByValue(self.low, self.high ,self.risk)
+        # print(' risk high low', self.high, self.low)
+        # reduce the list of sector based on the risk range
+        self.result = self.reduceByValue(self.low, self.high, self.risk)
 
-        print ('RISK', self.result)
+        print('RISK', self.result)
 
-        self.returns= {k: self.returns[k] for k in self.result if k in self.returns}
-        
+        self.returns = {k: self.returns[k] for k in self.result if k in self.returns}
 
         # create 2 groups of sector based on return
 
-        self.low = min (float(self.returns[k]) for k in self.returns)
-        self.high = max (float(self.returns[k]) for k in self.returns)
-        self.diff = (self.high - self.low)/2
-        
+        self.low = min(float(self.returns[k]) for k in self.returns)
+        self.high = max(float(self.returns[k]) for k in self.returns)
+        self.diff = (self.high - self.low) / 2
 
         # select range of risk based on user input
         self.high = self.low + self.diff
-        
 
-        #reduce the list of sector with self.highest return 
+        # reduce the list of sector with self.highest return
         self.result = list()
         for k, v in self.returns.items():
-                if float(v) >= self.high:
-                    self.result.append (k)
-        
+            if float(v) >= self.high:
+                self.result.append(k)
+
         return self.result
+        
+        
